cf821db3d8fa4cd1e5c361580addfaa2e8bab3d3
LHearen <lhearen@gmail.com>
2018-06-26 13:56:51 +0800

    chapter - 2 added

--- src/main/util/Output.java
+++ src/main/util/Output.java
@@ -2,14 +2,23 @@ package util;
 
 import static java.lang.System.out;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.Stream;
+
+import sun.misc.ClassLoaderUtil;
 
 public final class Output {
     private Output() {
@@ -31,5 +40,21 @@ public final class Output {
             ignored.printStackTrace();
         }
         return new ArrayList<>();
+    }
+
+    public static List<String> readFile() {
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        InputStream is = loader.getResourceAsStream("file.txt");
+        List<String> lines = new ArrayList<>();
+        try (BufferedReader br
+                     = new BufferedReader(new InputStreamReader(is))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                lines.add(line);
+            }
+        } catch (IOException e) {
+
+        }
+        return lines;
     }
 }
--- src/test/chap2/Sol_9.java
+++ src/test/chap2/Sol_9.java
@@ -1,18 +1,60 @@
 package test.chap2;
 
-import org.junit.Test;
+import static java.lang.System.out;
+import static java.util.stream.Collectors.toList;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
+
+import org.junit.Test;
 
 public class Sol_9 {
     @Test
     public void testListMerge() {
-
+        List<List<Integer>> listList = new ArrayList<>();
+        listList.add(new ArrayList<>(Arrays.asList(1, 2, 3, 4)));
+        listList.add(new ArrayList<>(Arrays.asList(5, 6, 7)));
+        mergeOne(listList.stream()).forEach(out::print);
+        out.println("****************");
+        mergeTwo(listList.stream()).forEach(out::print);
+        out.println("****************");
+        mergeThree(listList.stream()).forEach(out::print);
+        out.println("****************");
+        mergeFour(listList.stream()).forEach(out::print);
+        out.println("****************");
+        mergeFive(listList.stream()).forEach(out::print);
+        out.println("****************");
     }
 
-    private <T> ArrayList<T> mergeOne(Stream<ArrayList<T>> arrStream) {
-        return arrStream.flatMap(Arrays::)
+    private <T> List<T> mergeOne(Stream<List<T>> listStream) {
+        return listStream.flatMap(List::stream).collect(toList());
     }
+
+    private <T> List<T> mergeTwo(Stream<List<T>> listStream) {
+        List<T> result = new ArrayList<>();
+        listStream.forEach(result::addAll);
+        return result;
+    }
+
+    private <T> List<T> mergeThree(Stream<List<T>> listStream) {
+        return listStream.reduce(new ArrayList<>(), (l1, l2) -> {
+            l1.addAll(l2);
+            return l1;
+        });
+    }
+
+    private <T> List<T> mergeFour(Stream<List<T>> listStream) {
+        return listStream.reduce((l1, l2) -> {
+            List<T> l = new ArrayList<>(l1);
+            l.addAll(l2);
+            return l;
+        }).orElse(new ArrayList<>());
+    }
+
+    private <T> List<T> mergeFive(Stream<List<T>> listStream) {
+        return listStream.collect(ArrayList::new, List::addAll, List::addAll);
+    }
+
 }
