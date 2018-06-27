cfc6181bd760e8f7b045c21c17328b94ccfa1b53
LHearen <lhearen@gmail.com>
2018-06-27 08:40:59 +0800

    chap-3 partially added

---
+++ src/test/chap3/Sol_3.java
@@ -0,0 +1,26 @@
+package chap3;
+
+import java.util.function.BooleanSupplier;
+
+import org.junit.Test;
+
+public class Sol_3 {
+    @Test(expected = AssertionError.class)
+    public void testAssert() {
+        assertThat(4 == 5);
+        assertThat(() -> 4 == 5);
+    }
+
+    private void assertThat(boolean result) {
+        if (!result) {
+            throw new AssertionError("False Equation");
+        }
+    }
+
+    private void assertThat(BooleanSupplier booleanSupplier) {
+        if (!booleanSupplier.getAsBoolean()) {
+            throw new AssertionError("False Equation");
+        }
+    }
+
+}
