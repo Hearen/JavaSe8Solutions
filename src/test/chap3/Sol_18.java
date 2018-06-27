9755d05abbbfecefb24518443639e1ea0b3370c6
LHearen <lhearen@gmail.com>
2018-06-27 10:27:05 +0800

    chap-3 add more

---
+++ src/test/chap3/Sol_18.java
@@ -0,0 +1,57 @@
+package chap3;
+
+import static java.lang.System.out;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+import org.junit.Test;
+
+public class Sol_18 {
+    @Test
+    public void testFunctionWithEx() {
+        Function<String, Integer> getLen = uncheck((String s) -> getLength(s));
+        out.println(getLen.apply("Hearen"));
+        out.println(getLen.apply("Katherine"));
+        out.println(uncheckWithHandler((String s) -> getLength(s),
+                (e) -> out.println("Exception: " + e.getMessage()))
+                .apply("Katherine"));
+    }
+
+    private Integer getLength(String s) throws RuntimeException {
+        if (s.length() > 6) {
+            throw new RuntimeException("Longer than I expect");
+        }
+        return s.length();
+    }
+
+    private <T, R> Function<T, R> uncheck(FunctionWithEx<T, R> functionWithEx) {
+        return (t) -> {
+            R r = null;
+            try {
+                r = functionWithEx.apply(t);
+            } catch (Exception e) {
+                out.println(e.getMessage());
+            }
+            return r;
+        };
+    }
+
+    private <T, R> Function<T, R> uncheckWithHandler(FunctionWithEx<T, R> functionWithEx, Consumer<Throwable> handler) {
+        return (t) -> {
+            R r = null;
+            try {
+                r = functionWithEx.apply(t);
+            } catch (Exception e) {
+                handler.accept(e);
+            }
+            return r;
+        };
+    }
+
+
+    @FunctionalInterface
+    interface FunctionWithEx<T, U> {
+        U apply(T t) throws Exception;
+    }
+}
